a0724be3563dbfa22d1fc296065c8c4f4c77fd20
skoggy <erikskoglund88@gmail.com>
2017-02-01 00:28:35 +0100

    lights and tweaks

---
+++ Assets/_Project/Scripts/Effects/Wind.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour 
+{
+	public static float GlobalDirection = 0f;
+
+	public static Vector3 Direction
+	{
+		get{ return new Vector3(Mathf.Cos(GlobalDirection), 0f, Mathf.Sin(GlobalDirection)); }
+	}
+
+	public static float GetMagnitude(Vector3 position)
+	{
+		float scale = 1f;
+		return Mathf.PerlinNoise(position.x * scale, position.z * scale) / scale;
+	}
+
+	public float DirectionChangeSpeed = 0.1f;
+	public float CurrentDirection;
+
+	private float _directionChange;
+
+	void Start () 
+	{
+		GlobalDirection = UnityEngine.Random.Range(0f, Mathf.PI * 2f);
+	}
+	
+	void Update () 
+	{
+		GlobalDirection += _directionChange * Time.deltaTime;
+		WrapDirection();
+		_directionChange += DirectionChangeSpeed * 0.001f *  Time.deltaTime;
+		CurrentDirection = GlobalDirection;
+	}
+
+    private void WrapDirection()
+    {
+		var dir = GlobalDirection * Mathf.Rad2Deg;
+		while(dir > 360f)
+			dir -= 360f;
+		while(dir < 0f)
+			dir += 360f;
+		GlobalDirection = dir * Mathf.Deg2Rad;
+    }
+}
---
+++ Assets/_Project/Scripts/Effects/WindEffector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffector : MonoBehaviour 
+{
+	[Range(0f, 1f)]
+	public float Magnitude = 1f;
+	
+	void FixedUpdate () 
+	{
+		var direction = Wind.Direction;
+		var magnitude = Wind.GetMagnitude(transform.position) * Magnitude;
+		GetComponent<Rigidbody>().AddForce(direction * magnitude);
+	}
+}
